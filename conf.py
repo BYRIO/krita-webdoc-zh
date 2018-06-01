2e63865936f0f645f479e249829bcbfc189e2e0b
Scott Petrovic <scottpetrovic@gmail.com>
2018-06-01 06:33:44 -0500

    Add 404 redirect with an htaccess file. ignore adding when building epub

--- conf.py
+++ conf.py
@@ -89,6 +89,7 @@ language = 'en'
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_extra_path = ['.htaccess'] # copy over .htaccess file to each langaguge
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -329,7 +330,7 @@ else:
 epub_scheme = 'URL'
 
 # A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
+epub_exclude_files = ['search.html', '.htaccess']
 
 epub_tocscope = 'includehidden'
 
