9408303a85766c4135d4c44190057de2b33bc92a
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2018-06-06 16:29:30 +0200

    Update static messages.sh with new pot function.

--- StaticMessages.sh
+++ StaticMessages.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
 FILENAME="krita-docs"
+MULTI_POT=true
 
+# Dummy implementation of the function which just exports a single file.
 function export_pot_file # First parameter will be the path of the directory with the pot files, includes $FILENAME
+{
+    echo "Creating POT files"
+	potfile=$1
+	make gettext
+    echo "Moving KritaFAQ.pot file to $potfile"
+	mv _build/gettext/KritaFAQ.pot $potfile
+	echo "Removing gettext folder."
+	rm -rf _build/gettext
+}
+
+function export_pot_multi # First parameter will be the path of the directory with the pot files, $FILENAME becomes directory name
 {
     echo "Creating POT files"
 	potfile=$1
 	make gettext
     # Remove the pages that should not be translated.
     echo "Removing untranslatable pages."
-    rm -rf _build/gettext/contributors_manual/untranslatable_pages
+    rm -rf _build/gettext/untranslatable_pages.pot
     echo "Moving the pot files to $potfile"
 	mv _build/gettext $potfile
 	echo "Removing gettext folder."
